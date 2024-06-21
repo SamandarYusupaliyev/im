0128deb09445c50c20e2a81570099791a25287c9
SamandarYusupaliyev <samandaryusupaliyev9@gmail.com>
2024-06-21 12:56:48 +0500

    papka

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,28 +2,28 @@ function About() {
   return (
     <div className="carousel w-full">
   <div id="slide1" className="carousel-item relative w-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1625726411847-8cbb60cc71e6.jpg" className="w-full" />
+    <img src="https://optim.tildacdn.one/tild3733-6434-4663-a339-643565303235/-/format/webp/5.jpg" className="w-full h-[500px] object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide4" className="btn btn-circle">❮</a> 
       <a href="#slide2" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide2" className="carousel-item relative w-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1609621838510-5ad474b7d25d.jpg" className="w-full" />
+    <img src="https://optim.tildacdn.one/tild6538-6632-4932-a661-623235643531/-/format/webp/3.jpg" className="w-full h-[500px] object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide1" className="btn btn-circle">❮</a> 
       <a href="#slide3" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide3" className="carousel-item relative w-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1414694762283-acccc27bca85.jpg" className="w-full" />
+    <img src="https://optim.tildacdn.one/tild6166-3438-4165-b864-623861333332/-/format/webp/2.jpg" className="w-full h-[500px] object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide2" className="btn btn-circle">❮</a> 
       <a href="#slide4" className="btn btn-circle">❯</a>
     </div>
   </div> 
   <div id="slide4" className="carousel-item relative w-full">
-    <img src="https://img.daisyui.com/images/stock/photo-1665553365602-b2fb8e5d1707.jpg" className="w-full" />
+    <img src="https://optim.tildacdn.one/tild6135-3736-4561-b466-343339386236/-/format/webp/4.jpg" className="w-full h-[500px] object-cover" />
     <div className="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
       <a href="#slide3" className="btn btn-circle">❮</a> 
       <a href="#slide1" className="btn btn-circle">❯</a>
